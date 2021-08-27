905c1e8283a31c3d8e6bf2f9b42835e109e646fd
anjola85 <muhammedadeyemi@Muhammeds-MacBook-Pro.local>
2021-08-27 06:36:27 -0400

    added method to retrieve a user's timeline tweet

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,8 +1,7 @@
-from fetch_tweets import twitter_auth
 import tweepy
 from tweepy.streaming import StreamListener #listens to tweet based on certain keywords
 from tweepy import OAuthHandler #reponsible for authenticating based on env variables which are associated with the twitter app
-from tweepy import Stream 
+from tweepy import Stream, API, Cursor
 import sys,os, re
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -15,21 +14,69 @@ consumer_secret = os.environ.get("CONSUMER_SECRET_KEY")
 access_token = os.environ.get("ACCESS_TOKEN") 
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
+# Twitter Client
+class TwitterClient():
+    
+    def __init__(self, twitter_user=None):
+        # authenticate to properly communicate with the Twitter API
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+    
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+    
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+    
+    # def get_specific_tweet(self, num_tweets):
+    #     tweets = []
+    #     for tweet in Cursor(self.twitter_client.seach_tweets(q="(d|D)(o|O)(g|G)(e|E)"), id=self.twitter_user).items(num_tweets):
+    #         tweets.append(tweet)
+    #     return tweets
+        
+    
+
+class TwitterAuthenticator():
+    
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        return auth
+    
+
 class TwitterStreamer():
     """
     Class for streaming and processing live tweets.
     """
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+    
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter authentication and the connection to the Twitter Streaming API.
-        listener = StdOutListener()
-        auth = OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)    
+        listener = TwitterListener()
+        auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener) #listener which handles the return, can be data or error
+        
+        # This line filters Twitter Streams to capture data by keywords
         stream.filter(track=hash_tag_list)
         
 
 #this class will inherit from streamlistener class 
-class StdOutListener(StreamListener):
+class TwitterListener(StreamListener):
     """
     This is a basic listener class that just prints recieved tweets to stdout.
     """
@@ -51,6 +98,9 @@ class StdOutListener(StreamListener):
     
     # method that handles error
     def on_error(self, status):
+        if status == 420:
+            # returning False on data method in case rate limit occurs.
+            return False
         print(status)
         
         
@@ -59,7 +109,9 @@ if __name__ == "__main__":
     # hash_tag_lisy = []
     match_doge = "(d|D)(o|O)(g|G)(e|E)" #regex to match doge word
     fetched_tweets_filename = "tweets.json" #return in json format
+
+    twitter_client = TwitterClient('elonmusk')
+    print(twitter_client.get_specific_tweet(1))
     
-    # twitter streaner object
-    twitter_streaner = TwitterStreamer()
-    twitter_streaner.stream_tweets(fetched_tweets_filename, match_doge)
+    # twitter_streaner = TwitterStreamer()  
+    # twitter_streaner.stream_tweets(fetched_tweets_filename, match_doge)
